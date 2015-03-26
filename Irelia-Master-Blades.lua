8f23dd500d95fa081b0aca6eaa5da46f1ed61ce5
AMBER17 <amberhacking17@gmail.com>
2015-03-26 18:13:45 +0100

    Update Irelia-Master-Blades.lua

--- Irelia-Master-Blades.lua
+++ Irelia-Master-Blades.lua
@@ -1,6 +1,6 @@
 if myHero.charName ~= "Irelia" then return end 
 
-local  IreliaMasterBlades_Version = 1.7
+local  IreliaMasterBlades_Version = 1.71
 
 class "SxUpdate"
 function SxUpdate:__init(LocalVersion, Host, VersionPath, ScriptPath, SavePath, Callback)
@@ -80,8 +80,8 @@ if FileExist(LIB_PATH .. "/VPrediction.lua") then
 	if VP.version >= 3 then	
 		SxUpdate(0,
 			"raw.githubusercontent.com",
-			"/SidaBoL/Scripts/blob/master/Common/VPrediction.version",
-			"/SidaBoL/Scripts/blob/master/Common/VPrediction.lua",
+			"/SidaBoL/Scripts/master/Common/VPrediction.version",
+			"/SidaBoL/Scripts/master/Common/VPrediction.lua",
 			LIB_PATH.."/VPrediction.lua",
 			function(NewVersion) if NewVersion > 0 then print("<font color=\"#F0Ff8d\"><b>VPrediction: </b></font> <font color=\"#FF0F0F\">Updated to "..NewVersion..". Please Reload with 2x F9</b></font>") ForceReload = true end 
 		end)
@@ -89,8 +89,8 @@ if FileExist(LIB_PATH .. "/VPrediction.lua") then
 else
 	SxUpdate(0,
 		"raw.githubusercontent.com",
-		"/SidaBoL/Scripts/blob/master/Common/VPrediction.version",
-		"/SidaBoL/Scripts/blob/master/Common/VPrediction.lua",
+		"/SidaBoL/Scripts/master/Common/VPrediction.version",
+		"/SidaBoL/Scripts/master/Common/VPrediction.lua",
 		LIB_PATH.."/VPrediction.lua",
 		function(NewVersion) if NewVersion > 0 then print("<font color=\"#F0Ff8d\"><b>VPrediction: </b></font> <font color=\"#FF0F0F\">Updated to "..NewVersion..". Please Reload with 2x F9</b></font>") ForceReload = true end 
 	end)
